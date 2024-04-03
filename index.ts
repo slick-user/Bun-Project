1c739c3a428bc77879156f2887ccd5bbafffbdcf
unknown <you@example.com>
2024-04-03 13:05:44 +0500

    added the vercel schema

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const server = Bun.serve({
     fetch(req) {
         const url = new URL(req.url);
         if (url.pathname === "/") return new Response("Home Page!");
-        if (url.pathname === "/h") return new Response(Bun.file("./test.html"));
+        //if (url.pathname === "/h") return new Response(Bun.file("./test.html"));
         
         const body = figlet.textSync("Hello and Welcome to Bun! :D")
         return new Response(body);
@@ -21,4 +21,4 @@ const server = Bun.serve({
 
 });
 
-console.log(`Listening on LocalHost:${server.port}...`)
+console.log(`Listening on LocalHost yes yes:${server.port}...`)
