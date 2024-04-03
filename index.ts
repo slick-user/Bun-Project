f430147b4c7d16f7be2de0a87bc582aebc71f9e8
unknown <you@example.com>
2024-04-03 12:48:01 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import figlet from "figlet";
+
+const server = Bun.serve({
+    port: Bun.env.port || 3000,
+    fetch(req) {
+        const url = new URL(req.url);
+        if (url.pathname === "/") return new Response("Home Page!");
+        if (url.pathname === "/h") return new Response(Bun.file("./test.html"));
+        
+        const body = figlet.textSync("Hello and Welcome to Bun! :D")
+        return new Response(body);
+
+    },
+    error(error) {                                                      //Error Handler
+        return new Response(`<pre>${error}\n${error.stack}</pre>`, {
+            headers: {
+                "Content-Type": "text/html",
+            },
+        });
+    },
+
+});
+
+console.log(`Listening on LocalHost:${server.port}...`)
